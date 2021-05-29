9206b328f3bd1e3b82024445cb6fb5d7e675606b
Yuwei <61220119+c118173@users.noreply.github.com>
2021-05-30 02:39:57 +0800

    Update Week9 Q&A.py

--- Week9/Week9 Q&A.py
+++ Week9/Week9 Q&A.py
@@ -192,3 +192,138 @@ class Link:
             s = s + str(self.first) + ', '
             self = self.rest
         return s + str(self.first) + '>'
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def map(self, fn):
+        """
+        Apply a function `fn` to each node in the tree and mutate the tree.
+
+        >>> t1 = Tree(1)
+        >>> t1.map(lambda x: x + 2)
+        >>> t1.map(lambda x : x * 4)
+        >>> t1.label
+        12
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> t2.map(lambda x: x * x)
+        >>> t2
+        Tree(9, [Tree(4, [Tree(25)]), Tree(16)])
+        """
+        self.label = fn(self.label)
+        for b in self.branches:
+            b.map(fn)
+
+    def __contains__(self, e):
+        """
+        Determine whether an element exists in the tree.
+
+        >>> t1 = Tree(1)
+        >>> 1 in t1
+        True
+        >>> 8 in t1
+        False
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> 6 in t2
+        False
+        >>> 5 in t2
+        True
+        """
+        if self.label == e:
+            return True
+        for b in self.branches:
+            if e in b:
+                return True
+        return False
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+
+def order(redwood):
+    """Return a list containing a valid plucking order for the labels of t.
+    A plucking order contains all labels of t, and the label for each node of t appears after the labels of all its descendant nodes.
+    """
+    plucking_order = []
+    for b in redwood.branches:
+        plucking_order.extend(order(b))
+    return plucking_order + [redwood.label]
+
+
+def pluck(pine):
+    """Return a function that returns whether a plucking order is valid for a number tree t when called repeatedly on elements of a plucking order.
+    """
+    def plucker(k):
+        def pluck_one_leaf(cyprus):
+            """Return a copy of cyprus without leaf k and check that k is a 
+            leaf label, not an interior node label.
+            """
+            # Assuming cyprus is not a leaf itself
+            if cyprus.label == k:
+                return 'Hey, not valid!'
+            plucked_branches = []
+            for b in cyprus.branches:
+                skip_this_leaf = b.is_leaf() and b.label == k
+                if not skip_this_leaf:
+                    plucked_branches_or_error = pluck_one_leaf(b)
+                    if isinstance(plucked_branches_or_error, str):
+                        return plucked_branches_or_error
+                    else:
+                        plucked_branches.append(plucked_branches_or_error)
+            return Tree(cyprus.label, plucked_branches)
+
+        nonlocal pine
+        if pine.is_leaf():
+            assert k == pine.label, 'all k must appear in pine'
+            return 'success!'
+        pine = pluck_one_leaf(pine)
+        if isinstance(pine, str): #if cause trouble
+            return pine
+        else:
+            return plucker
+
+    return plucker
+
+
+def lookups(k, key):
+    """Yield one lookup function for each node of k that has the label key.
+    """
+    if key == k.label:
+        yield lambda v: v.label
+    for i in range(len(k.branches)):
+        for lookup in lookups(k.branches[i], key):
+            yield new_lookup(i, lookup)
+
+def new_lookup(i, f): #assuming f is a lookup function for a branches of v
+    def g(v):
+        return f(v.branches[i])
+    return g
