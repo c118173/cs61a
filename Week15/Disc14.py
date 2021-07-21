f7973be0d28bfdf67a2beb26e3fd8a6d349e114b
Yuwei <61220119+c118173@users.noreply.github.com>
2021-07-21 16:20:41 +0800

    Add files via upload

---
+++ Week15/Disc14.py
@@ -0,0 +1,309 @@
+# Recursion
+
+def paths(x, y):
+    """Return a list of ways to reach y from x by repeated
+    incrementing or doubling.
+    >>> paths(3, 5)
+    [[3, 4, 5]]
+    >>> sorted(paths(3, 6))
+    [[3, 4, 5, 6], [3, 6]]
+    >>> sorted(paths(3, 9))
+    [[3, 4, 5, 6, 7, 8, 9], [3, 4, 8, 9], [3, 6, 7, 8, 9]]
+    >>> paths(3, 3) # No calls is a valid path
+    [[3]]
+    """
+    if y == x:
+        return [[x]]
+    elif y < x:
+        return []
+    else:
+        a = paths(x+1, y)
+        b = paths(2*x, y)
+        return [[x] + elem for elem in a + b]
+
+
+def merge(s1, s2):
+    """ Merges two sorted lists """
+    if len(s1) == 0:
+        return s2
+    elif len(s2) == 0:
+        return s1
+    elif s1[0] < s2[0]:
+        return [s1[0]] + merge(s1[1:], s2)
+    else:
+        return [s2[0]] + merge(s1, s2[1:])
+
+
+def mergesort(seq):
+    """ Merge sort."""
+    l = len(seq)
+    if l <= 1:
+        return seq
+    return merge(mergesort(seq[: l//2]), mergesort(seq[l//2:]))
+
+
+# Trees
+
+def long_paths(tree, n):
+    """Return a list of all paths in tree with length at least n.
+    >>> t = Tree(3, [Tree(4), Tree(4), Tree(5)])
+    >>> left = Tree(1, [Tree(2), t])
+    >>> mid = Tree(6, [Tree(7, [Tree(8)]), Tree(9)])
+    >>> right = Tree(11, [Tree(12, [Tree(13, [Tree(14)])])])
+    >>> whole = Tree(0, [left, Tree(13), mid, right])
+    >>> for path in long_paths(whole, 2):
+    ...     print(path)
+    ...
+    <0 1 2>
+    <0 1 3 4>
+    <0 1 3 4>
+    <0 1 3 5>
+    <0 6 7 8>
+    <0 6 9>
+    <0 11 12 13 14>
+    >>> for path in long_paths(whole, 3):
+    ...     print(path)
+    ...
+    <0 1 3 4>
+    <0 1 3 4>
+    <0 1 3 5>
+    <0 6 7 8>
+    <0 11 12 13 14>
+    >>> long_paths(whole, 4)
+    [Link(0, Link(11, Link(12, Link(13, Link(14)))))]
+    """
+    paths = []
+    if n <= 0 and tree.is_leaf():
+        paths.append(Link(tree.label))
+    for b in tree.branches:
+        for p in long_paths(b, n-1):
+            paths.append(Link(tree.label, p))
+    return paths
+
+
+def widest_level(t):
+    """
+    >>> sum([[1], [2]], [])
+    [1, 2]
+    >>> t = Tree(3, [Tree(1, [Tree(1), Tree(5)]),
+    ... Tree(4, [Tree(9, [Tree(2)])])])
+    >>> widest_level(t)
+    [1, 5, 9]
+    """
+    levels = []
+    x = [t]
+    while not t.is_leaf():
+        levels.append([t.label for t in x])
+        x = sum([t.branches for t in x], [])
+    return max(levels, key=len)
+
+
+# Mutation
+
+def bake(banana, bread):
+    banana.append(bread.append(1)) # This line is Multiple Choice
+    # Select all correct answers for the blank above
+    # A. banana.append(bread.append(1))
+    # B. bread.append(banana.append(1))
+    # C. banana.extend([bread.extend([1])])
+    # D. bread.extend([banana.extend([1])])
+    bread += banana[: (len(banana) - 1)]
+    banana.extend(bread[bread[1]])
+    return banana, bread
+
+s = [1]
+banana, bread = bake(s, [7, 2, s])
+
+
+# Mutable Linked Lists
+
+def remove_duplicates(lnk):
+    """
+    >>> lnk = Link(1, Link(1, Link(1, Link(1, Link(5)))))
+    >>> remove_duplicates(lnk)
+    >>> lnk
+    Link(1, Link(5))
+    """
+    while lnk is not Link.empty and lnk.rest is not Link.empty:
+        if lnk.first == lnk.rest.first:
+            lnk.rest = lnk.rest.rest
+        else:
+            lnk = lnk.rest
+        
+
+# OOP
+
+class Emotion:
+
+    num = 0
+
+    def __init__(self):
+        self.power = 5
+
+    def feeling(self, other):
+        if self.power == other.power:
+            print("Together")
+        elif self.power > other.power:
+            self.catchphrase()
+            other.catchphrase()
+        else:
+            other.catchphrase()
+            self.catchphrase()
+
+class Joy(Emotion):
+
+    def __init__(self):
+        Emotion.__init__(self)
+        Emotion.num += 1
+
+    def catchphrase(self):
+        print("Think positive thoughts")
+
+class Sadness(Emotion):
+
+    def __init__(self):
+        Emotion.__init__(self)
+        Emotion.num += 1
+
+    def catchphrase(self):
+        print("I'm positive you will get lost")
+
+
+# Generators
+
+def repeated(f):
+    """
+    >>> double = lambda x: 2 * x
+    >>> funcs = repeated(double)
+    >>> identity = next(funcs)
+    >>> double = next(funcs)
+    >>> quad = next(funcs)
+    >>> oct = next(funcs)
+    >>> quad(1)
+    4
+    >>> oct(1)
+    8
+    >>> [g(1) for _, g in
+    ... zip(range(5), repeated(lambda x: 2 * x))]
+    [1, 2, 4, 8, 16]
+    """
+    g = lambda x: x
+    while True:
+        yield g
+        g = (lambda g: lambda x: f(g(x)))(g)
+
+
+from operator import add, mul
+
+def accumulate(iterable, f):
+    """
+    >>> list(accumulate([1, 2, 3, 4, 5], add))
+    [1, 3, 6, 10, 15]
+    >>> list(accumulate([1, 2, 3, 4, 5], mul))
+    [1, 2, 6, 24, 120]
+    """
+    it = iter(iterable)
+    total = next(it)
+    yield total
+    for elem in it:
+        total = f(total, elem)
+        yield total
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def map(self, fn):
+        """
+        Apply a function `fn` to each node in the tree and mutate the tree.
+
+        >>> t1 = Tree(1)
+        >>> t1.map(lambda x: x + 2)
+        >>> t1.map(lambda x : x * 4)
+        >>> t1.label
+        12
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> t2.map(lambda x: x * x)
+        >>> t2
+        Tree(9, [Tree(4, [Tree(25)]), Tree(16)])
+        """
+        self.label = fn(self.label)
+        for b in self.branches:
+            b.map(fn)
+
+    def __contains__(self, e):
+        """
+        Determine whether an element exists in the tree.
+
+        >>> t1 = Tree(1)
+        >>> 1 in t1
+        True
+        >>> 8 in t1
+        False
+        >>> t2 = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+        >>> 6 in t2
+        False
+        >>> 5 in t2
+        True
+        """
+        if self.label == e:
+            return True
+        for b in self.branches:
+            if e in b:
+                return True
+        return False
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+
+class Link:
+
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
